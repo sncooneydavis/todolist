5fccfda54eac123139050083c9dc7c95a16c3c12
sncooneydavis <portland.pickle.chip@gmail.com>
2024-12-05 15:40:32 -0500

    setup: environment

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+// webpack.common.js
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ESLintWebpackPlugin from "eslint-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+	entry: "./src/index.js",
+	output: {
+		filename: "scripts/[name].js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+		}),
+		new ESLintWebpackPlugin({ 
+			extensions: ["js", "jsx"],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+				generator: { filename: "assets/images/[name][ext]", },
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+				generator: { filename: "assets/fonts/[name][ext]", },
+			},
+		],
+	},
+};
---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+// webpack.prod.js
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+
+export default merge(common, {
+	mode: 'production',
+	devtool: 'source-map',
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		],
+	},
+	optimization: {
+		minimizer: [
+			new CssMinimizerPlugin(),
+		],
+	},
+});
