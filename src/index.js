dc181cfa8ab042437cc33e47e4ca0c1e1c638c2a
sncooneydavis <portland.pickle.chip@gmail.com>
2024-12-13 14:36:26 -0500

    build: organization of template modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import './pages/list-view/left-panel/1-structure-templates.html';
+import './pages/list-view/left-panel/2-dragbar-template.html';
+import './pages/list-view/left-panel/3-list-template.html';
+import './pages/list-view/left-panel/list-items/1.task-template.html';
+import './pages/list-view/left-panel/list-items/2.event-template.html';
+import './pages/list-view/left-panel/list-items/3.components-templates.html';
+import './pages/list-view/left-panel/list-items/editing/due-templates.html';
+// add other editing templates here 
+import './pages/list-view/quick-add-templates.html';
+
+// populate inbox list
+
+// populate 
